5f5f22ce415b4bb66ebd8c849db7f7ec92212a6a
BenJThornton <91638449+BenJThornton@users.noreply.github.com>
2021-10-17 20:52:18 -0700

    commit

---
+++ fizzbuzz.js
@@ -0,0 +1,14 @@
+var x="Fizz"
+var y="Buzz"
+
+for(var i =1; i<=100; i++)
+    if(i%15==0){
+        console.log(x+y)
+    }
+    if(i%3==0){
+        console.log(x)
+    }
+    if(i%5==0){
+        console.log(y)
+    }
+else (console.log(i))
